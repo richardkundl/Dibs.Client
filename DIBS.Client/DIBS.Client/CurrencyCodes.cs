16a457ea8242a68d92285a5f7ffe6cf32aac0046
Richard Kundl <kundl.richard@gmail.com>
2013-09-10 20:46:50 +0200

    add currency codes

---
+++ DIBS.Client/DIBS.Client/CurrencyCodes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DIBS.Client
+{
+    public static class CurrencyCodes
+    {
+        /// <summary>
+        /// DKK
+        /// </summary>
+        public static string DanishKroner { get { return "208"; } }
+
+        /// <summary>
+        /// EUR
+        /// </summary>
+        public static string Euro { get { return "978"; } }
+
+        /// <summary>
+        /// USD
+        /// </summary>
+        public static string UsDollar { get { return "840"; } }
+
+        /// <summary>
+        /// GBP
+        /// </summary>
+        public static string EnglishPound { get { return "826"; } }
+
+        /// <summary>
+        /// SEK
+        /// </summary>
+        public static string SwedishKroner { get { return "752"; } }
+
+        /// <summary>
+        /// AUD
+        /// </summary>
+        public static string AustralianDollar { get { return "036"; } }
+
+        /// <summary>
+        /// CAD
+        /// </summary>
+        public static string CanadianDollar { get { return "124"; } }
+        /// <summary>
+        /// ISK
+        /// </summary>
+        public static string IcelandicKroner { get { return "352"; } }
+
+        /// <summary>
+        /// JPY
+        /// </summary>
+        public static string JapaneseYen { get { return "392"; } }
+
+        /// <summary>
+        /// NZD
+        /// </summary>
+        public static string NewZealandDollar { get { return "554"; } }
+
+        /// <summary>
+        /// NOK
+        /// </summary>
+        public static string NorwegianKroner { get { return "578"; } }
+
+        /// <summary>
+        /// CHF
+        /// </summary>
+        public static string SwissFranc { get { return "756"; } }
+        /// <summary>
+        /// TRY
+        /// </summary>
+        public static string TurkishLire { get { return "949"; } }
+    }
+}
